a0d8184da2c136f6ee004a6bed525318d6b3b41a
Aleksander Shishov <alexshishg@gmail.com>
2023-03-11 22:08:09 +0300

    continue 11-03-2023

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import { useEffect, useState } from "react";
 
 function App() {
 
-const [task, setTask] = useState([])
+  const [task, setTask] = useState([])
 
-  useEffect( () => {
-  const getTasks = async () => {
-    const tasksCol = collection(db, 'task');
-    const snapshot = await getDocs(tasksCol);
-    const taskList = snapshot.docs.map(doc => doc.data());
-    return taskList;
-  }
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksCol = collection(db, 'task');
+      const snapshot = await getDocs(tasksCol);
+      const taskList = snapshot.docs.map(doc => doc.data());
+      return taskList;
+    }
     getTasks()
   })
   return (
-    <div className="App">
-<ul>
-  { task.map(task => )}
-</ul>
-    </div>
+      <div className="App">
+        <ul>
+          {task.map(task => (
+              <li>
+                key={task.title}>{task.title}
+              </li>
+          ))}
+        </ul>
+      </div>
   );
 }
 
